8558a2c17dbdcde1b25faa540aca4edd1102200c
MD ABUL HOSSAIN <harigov63@gmail.com>
2026-09-07 16:13:40 +0600

    Implement OAuth2 authentication handler

---
+++ src/app/mod.rs
@@ -0,0 +1,240 @@
+use std::path::PathBuf;
+use std::result::Result as StdResult;
+use std::{env, path};
+
+use anyhow::Error as AnyError;
+use chrono::Utc;
+use reqwest::Client as HttpClient;
+use serde_derive::{Deserialize, Serialize};
+
+mod credentials;
+mod errors;
+
+use errors::{AuthError, Result};
+
+const GRANT_TYPE: &str = "authorization_code";
+const DEFAULT_APP_NAME: &str = "gauth_app";
+const TOKEN_DIR: &str = "GAUTH_TOKEN_DIR";
+const GOOGLE_VALIDATE_HOST: &str = "https://www.googleapis.com";
+
+type AuthHandler = Box<dyn AuthHandlerFn>;
+
+pub trait AuthHandlerFn: Fn(String) -> StdResult<String, AnyError> + 'static + Send + Sync {}
+
+impl<H> AuthHandlerFn for H where
+    H: Fn(String) -> StdResult<String, AnyError> + 'static + Send + Sync
+{
+}
+
+pub struct Auth {
+    app_name: String,
+    auth_handler: Option<AuthHandler>,
+    oauth_creds: credentials::OauthCredentials,
+    consent_uri: String,
+    token_validate_host: String,
+    http_client: HttpClient,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Token {
+    access_token: String,
+    expires_in: u64,
+    refresh_token: Option<String>,
+    scope: Option<String>,
+    token_type: String,
+    expires_at: Option<u64>,
+}
+
+impl Token {
+    fn bearer_token(&self) -> String {
+        format!("{} {}", self.token_type, self.access_token)
+    }
+
+    fn is_expired(&self) -> bool {
+        match self.expires_at {
+            Some(expires_at) => expires_at < Utc::now().timestamp() as u64,
+            None => true,
+        }
+    }
+
+    fn set_expires_at(&mut self) {
+        self.expires_at = Some(Utc::now().timestamp() as u64 + self.expires_in);
+    }
+}
+
+impl Auth {
+    pub fn from_file(key_path: &str, scopes: Vec<&str>) -> Result<Self> {
+        let kp = path::Path::new(key_path);
+        let oauth_creds = credentials::read_oauth_config(kp)?.installed;
+
+        let scope = scopes.join(" ");
+        let consent_uri = credentials::auth_code_uri_str(&oauth_creds, &scope)?;
+
+        Ok(Self {
+            app_name: DEFAULT_APP_NAME.to_owned(),
+            auth_handler: None,
+            oauth_creds,
+            consent_uri,
+            token_validate_host: GOOGLE_VALIDATE_HOST.to_owned(),
+            http_client: HttpClient::new(),
+        })
+    }
+
+    pub fn app_name(mut self, app_name: &str) -> Self {
+        self.app_name = app_name.to_owned();
+        self
+    }
+
+    pub fn handler<H: AuthHandlerFn>(mut self, handler: H) -> Self {
+        self.auth_handler = Some(Box::new(handler));
+        self
+    }
+
+    async fn generate_new_token(&self) -> Result<Token> {
+        let auth_code = match self.auth_handler.as_ref() {
+            Some(h) => (h)(self.consent_uri.clone()),
+            None => default_auth_handler(self.consent_uri.clone()),
+        }?;
+
+        self.exchange_auth_code(auth_code)
+            .await
+            .and_then(|token| self.cache_token(token))
+    }
+
+    pub async fn access_token(&self) -> Result<String> {
+        let token = match self.cached_token() {
+            Ok(token) => token,
+            Err(_) => self.generate_new_token().await?,
+        };
+
+        if self.is_token_valid(&token).await {
+            return Ok(token.bearer_token());
+        }
+
+        self.refresh_token(token)
+            .await
+            .and_then(|token| self.cache_token(token))
+            .map(|token| token.bearer_token())
+    }
+
+    #[cfg(feature = "app-blocking")]
+    pub fn access_token_blocking(&self) -> Result<String> {
+        futures::executor::block_on(self.access_token())
+    }
+
+    async fn exchange_auth_code(&self, auth_code: String) -> Result<Token> {
+        let req_builder = self
+            .http_client
+            .post(self.oauth_creds.token_uri.as_str())
+            .form(&[
+                ("code", auth_code.as_str()),
+                ("client_id", self.oauth_creds.client_id.as_str()),
+                ("client_secret", self.oauth_creds.client_secret.as_str()),
+                ("redirect_uri", self.oauth_creds.redirect_uri()?.as_str()),
+                ("grant_type", GRANT_TYPE),
+            ]);
+
+        let res = match req_builder.send().await {
+            Ok(resp) => resp,
+            Err(err) => return Err(AuthError::ReqwestError(err)),
+        };
+
+        let token = match res.json::<Token>().await {
+            Ok(token) => token,
+            Err(err) => return Err(AuthError::ReqwestError(err)),
+        };
+
+        Ok(token)
+    }
+
+    async fn refresh_token(&self, token: Token) -> Result<Token> {
+        let refresh_token_str = token
+            .refresh_token
+            .as_ref()
+            .ok_or(AuthError::RefreshTokenValue)?
+            .as_str();
+
+        let req_builder = self
+            .http_client
+            .post(self.oauth_creds.token_uri.as_str())
+            .form(&[
+                ("refresh_token", refresh_token_str),
+                ("client_id", self.oauth_creds.client_id.as_str()),
+                ("client_secret", self.oauth_creds.client_secret.as_str()),
+                ("grant_type", "refresh_token"),
+            ]);
+
+        let res = match req_builder.send().await {
+            Ok(resp) => resp,
+            Err(err) => return Err(AuthError::ReqwestError(err)),
+        };
+
+        let mut token = match res.json::<Token>().await {
+            Ok(token) => token,
+            Err(err) => return Err(AuthError::ReqwestError(err)),
+        };
+
+        token.refresh_token = Some(refresh_token_str.to_owned());
+        Ok(token)
+    }
+
+    fn cached_token(&self) -> Result<Token> {
+        let token_dir = self.token_dir()?;
+        let b = std::fs::read(token_dir.join("access_token.json"))?;
+        Ok(serde_json::from_slice::<Token>(&b)?)
+    }
+
+    fn cache_token(&self, token: Token) -> Result<Token> {
+        let token_dir = self.token_dir()?;
+
+        if !token_dir.exists() {
+            std::fs::create_dir_all(&token_dir)?;
+        }
+
+        let mut token = token;
+        token.set_expires_at();
+
+        let token_path = token_dir.join("access_token.json");
+        let b = serde_json::to_vec(&token)?;
+        std::fs::write(token_path, b)?;
+
+        Ok(token)
+    }
+
+    fn token_dir(&self) -> Result<PathBuf> {
+        if let Ok(token_dir) = env::var(TOKEN_DIR) {
+            Ok(PathBuf::from(token_dir))
+        } else {
+            match dirs::home_dir() {
+                Some(d) => Ok(d.join(format!(".{}", self.app_name))),
+                None => Err(AuthError::HomeDirError),
+            }
+        }
+    }
+
+    async fn is_token_valid(&self, token: &Token) -> bool {
+        if token.is_expired() {
+            return false;
+        }
+
+        let url = format!(
+            "{}/oauth2/v3/tokeninfo?access_token={}",
+            self.token_validate_host, token.access_token
+        );
+
+        match self.http_client.get(url.as_str()).send().await {
+            Ok(resp) => resp.status().is_success(),
+            Err(_) => false,
+        }
+    }
+}
+
+fn default_auth_handler(consent_uri: String) -> StdResult<String, AnyError> {
+    println!("> open the link in browser\n\n{}\n", consent_uri);
+    println!("> enter the auth. code\n");
+
+    let mut auth_code = String::new();
+    std::io::stdin().read_line(&mut auth_code)?;
+
+    Ok(auth_code)
+}
